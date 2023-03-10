922625dd6197549c9d10ccc6956d95318b540655
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-10 12:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+const input = document.getElementById('newTodoInput');
+const btn = document.getElementById('addTodoBtn');
+const list = document.getElementById('todoList');
+
+function myFunction(){
+   const li = document.createElement('li');
+	list.appendChild(li).innerHTML = input.value;
+}
 
