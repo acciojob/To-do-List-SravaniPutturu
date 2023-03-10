e7191dc2a7f94b62ee881e91d532467614f41290
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-10 17:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //your code here
 const input = document.getElementById('newTodoInput');
-const list = document.getElementById('todoList');
+const ol = document.getElementById('todoList');
 
 document.getElementById('addTodoBtn').onclick=function(){
 	if(input.value.length == 0){
 		alert("Please fill to do list.")
 	}else {
 	const li = document.createElement('li');
-	list.appendChild(li).innerHTML = input.value;
+		li.innerHTML = input.value;
+	ol.appendChild(li);
 	input.value = "";
 	}
 
