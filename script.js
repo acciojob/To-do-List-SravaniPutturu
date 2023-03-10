1be394a539176d283d472d8561c72630c7c9a9e1
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-10 12:51:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const list = document.getElementById('todoList');
 document.getElementById('addTodoBtn').onclick=function(){
 	if(input.value == ""){
 		alert("Please fill to do list.")
-	}
+	}else{
 	const li = document.createElement('li');
 	list.appendChild(li).innerHTML = input.value;
+	input.value = "";
 	}
+	return;
 }
 
