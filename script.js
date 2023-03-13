9295b100c84b09007f3f675f5ec21cdb29d104d0
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-13 20:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const todoValue = document.getElementById('newTodoInput');
 const ol = document.getElementById('todoList');
 const btn = document.getElementById('addTodoBtn')
 function addTodo(){
-	if(todoValue.value){
+	if(todoValue.value != " "){
 	const li = document.createElement('li');
 		li.textContent = todoValue.value;
 	ol.appendChild(li);
