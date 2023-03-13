fac4694842f32c5227eddd024f273f1d53db2020
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-13 20:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const input = document.getElementById('newTodoInput');
 const ol = document.getElementById('todoList');
 
 document.getElementById('addTodoBtn').onclick=function(){
-	if(input.value.trim().length == 0){
-		alert("Please fill to do list.")
-	}else {
+	if(input.value){
 	const li = document.createElement('li');
 		li.innerHTML = input.value;
 	ol.appendChild(li);
