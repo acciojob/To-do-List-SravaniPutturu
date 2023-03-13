cb68aafa9afd683f04a7bb6bf7e1551a695762e9
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-03-13 20:49:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your code here
-const input = document.getElementById('newTodoInput');
+const todoValue = document.getElementById('newTodoInput');
 const ol = document.getElementById('todoList');
 const btn = document.getElementById('addTodoBtn')
 function addTodo(){
-	if(input.value){
+	if(todoValue.value){
 	const li = document.createElement('li');
-		li.innerHTML = input.value;
+		li.textContent = todoValue.value;
 	ol.appendChild(li);
 	input.value = "";
 	}
